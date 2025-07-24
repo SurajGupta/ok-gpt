58464fb13fa3b66a00bbc3ffde2f099648391e6e
Suraj Gupta <SurajGupta@users.noreply.github.com>
2025-07-24 09:26:56 -0400

    more fixes

--- recorder.py
+++ recorder.py
@@ -5,78 +5,78 @@ import wave
 import os
 import numpy as np
 
-whisper_model = whisper.load_model("base")
 ambient_detected = False
-speech_volume = 50
+rms_that_indicates_speech = 50
 
-def live_speech(wait_time=50):
+WHISPER_MODEL = whisper.load_model("tiny.en")
+
+FRAMES_PER_SECOND = 16000 # 16000 Hz
+FRAMES_PER_BUFFER = 4000  # 4000 / 16000 Hz  =  250ms @ 16kHz microphone read
+
+def live_speech(wake_word_max_length_in_seconds=3):
     global ambient_detected
-    global speech_volume
+    global rms_that_indicates_speech
+
+    seconds_per_buffer = FRAMES_PER_BUFFER/FRAMES_PER_SECOND
 
     audio = pyaudio.PyAudio()
 
+    # Open the audio stream and start recording right away.
     stream = audio.open(
         format=pyaudio.paInt16,
         channels=1,
-        rate=16000,
+        rate=FRAMES_PER_SECOND,
         input=True,
-        frames_per_buffer=4000,
-        start=True,                        # start right away
+        frames_per_buffer=FRAMES_PER_BUFFER,
+        start=True,
     )
 
     frames = []
+
     recording = False
-    frames_recorded = 0
+    recorded_seconds = 0
 
     while True:
-        frames_recorded += 1
-        data = stream.read(4000)
-        rms = audioop.rms(data, 2)
-        print(f"RMS is {rms}")
+        # Read from the input/mic stream.  
+        # Will wait until buffer is full before returning.
+        recording = stream.read(FRAMES_PER_BUFFER)
+        recorded_seconds += seconds_per_buffer
+
+        # Calculate RMS of recording to quantify the loudness
+        rms_of_recording = audioop.rms(recording, 2)
+
+        print(f"RMS is {rms_of_recording}")
+
+        # At startup, determine the ambient sound level and use that to determine
+        # what the level will be considered speech
         if not ambient_detected:
-            if frames_recorded < 40:
-                if frames_recorded == 1:
+            if recorded_seconds < 1:
+                if recorded_seconds == seconds_per_buffer:
                     print("Detecting ambient noise...")
-                if frames_recorded > 5:
-                    if speech_volume < rms:
-                        speech_volume = rms
-                        print(f"speech_volume is {speech_volume}")
+                else
+                    if rms_that_indicates_speech < rms_of_recording:
+                        rms_that_indicates_speech = rms_of_recording
+                        print(f"RMS that indicates speech is {rms_that_indicates_speech}")
                 continue
-            elif frames_recorded == 40:
+            elif recorded_seconds == 1:
                 print("Listening...")
-                speech_volume = speech_volume * 3
+                rms_that_indicates_speech = rms_that_indicates_speech * 2.5
                 ambient_detected = True
 
-        if rms > speech_volume:
-            recording = True
-            frames_recorded = 0
-            print("recording")
-        elif recording and frames_recorded > wait_time:
-            recording = False
-
-            # wf = wave.open("audio.wav", 'wb')
-            # wf.setnchannels(1)
-            # wf.setsampwidth(audio.get_sample_size(pyaudio.paInt16))
-            # wf.setframerate(16000)
-            # wf.writeframes(b''.join(frames))
-            # wf.close()
-            #result = whisper_model.transcribe(
-            #    "audio.wav",
-            #    fp16=False
-            #)
-            #os.remove("audio.wav")
-
-            pcm = b''.join(frames)
-            the_audio = np.frombuffer(pcm, dtype=np.int16)
-            the_audio = the_audio.astype(np.float32) / 32768.0
-            result  = whisper_model.transcribe(the_audio, fp16=False)
-
-            yield result["text"].strip()
-
-            frames = []
-
         if recording:
-            frames.append(data)
+            frames.append(recording)
+            if (recorded_seconds >= wake_word_max_length_in_seconds):
+                recording = False
+                pcm = b''.join(frames)
+                the_audio = np.frombuffer(pcm, dtype=np.int16)
+                the_audio = the_audio.astype(np.float32) / 32768.0
+                result  = WHISPER_MODEL.transcribe(the_audio, fp16=False)
+                yield result["text"].strip()
+                frames = []
+        elif (rms_of_recording > rms_that_indicates_speech):
+            recording = True
+            recorded_seconds = seconds_per_buffer
+            print("recording")
 
     # TODO: do these when breaking from generator
     stream.stop_stream()
