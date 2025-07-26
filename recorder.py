462c274766d0ce2348f9648d4bbe859d89cdfad9
Suraj Gupta <SurajGupta@users.noreply.github.com>
2025-07-25 21:00:57 -0400

    fdsa

--- recorder.py
+++ recorder.py
@@ -130,7 +130,7 @@ def listen_for_and_transcribe_potential_wake_word(
             
             # Output computed decibles, recording status, and last transcription.
             if verbose:
-                _write_transcription_verbose_output(decibles, is_recording, print_sample_number_when_verbose, sample_number)
+                _write_transcription_verbose_output(decibles, is_recording, print_sample_number_when_verbose, sample_number, recorded_text)
 
             # If we are recording then determine if we are done recording.
             if is_recording:
@@ -203,7 +203,7 @@ def _calculate_decibles(recorded_input_data, offset_to_computed_decibles):
 
     return decibles
 
-def _write_transcription_verbose_output(decibles, is_recording, print_sample_number_when_verbose, sample_number):
+def _write_transcription_verbose_output(decibles, is_recording, print_sample_number_when_verbose, sample_number, recorded_text):
     decible_meter = _render_decible_meter(round(decibles))
 
     recording_state = (" " * int(((DECIBLE_METER_BAR_WIDTH - 15)/2))) + "<< recording >>" if is_recording else ""
