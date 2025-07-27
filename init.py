57d0baa4cd86c83737e4fff0a34da70633e4d8c2
Suraj Gupta <SurajGupta@users.noreply.github.com>
2025-07-26 21:48:16 -0400

    fdsa

--- init.py
+++ init.py
@@ -1,6 +1,6 @@
 import json
 
-from recorder import calibrate_decibles, listen_for_and_transcribe_potential_wake_words, establish_wake_words
+from recorder import calibrate_decibles, establish_wake_words
 
 # calibrate_decibles(65)
 establish_wake_words(65)
