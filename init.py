6eb2904c68a13b8ec7d883c517a248c83e0f6614
Suraj Gupta <SurajGupta@users.noreply.github.com>
2025-07-28 09:56:17 -0400

    fdsa

--- init.py
+++ init.py
@@ -1,8 +1,8 @@
 import json
 
-from recorder import calibrate_decibles, establish_wake_words, wait_for_wake_words
+from recorder import establish_wake_words, wait_for_wake_words
 
 # calibrate_decibles(65)
 print ("waiting for wake word...")
-wait_for_wake_words(65, decibles_that_indicate_speech = 50, verbose = True)
+establish_wake_words()
 print ("wake word found!")
